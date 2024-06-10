ff9d4b323fcb0f6e381374f331cadbe7336e7cb9
Xavier Mora <xavieralexander.moraperaza@usp.ceu.es>
2024-06-10 18:24:12 +0200

    metodo post en el cliente para albums

--- cliente/routes/albums.js
+++ cliente/routes/albums.js
@@ -7,7 +7,6 @@ const apiClient = axios.create({
   baseURL,
 });
 
-
 router.get('/', async (req, res) => {
   // Obtener el valor de `next` de la consulta
   let next = req.query.next;
@@ -20,7 +19,7 @@ router.get('/', async (req, res) => {
   
   try {
     // Realizar la llamada a la API para obtener los álbumes
-    const response = await apiClient.get(url);;
+    const response = await apiClient.get(url);
     const albums = response.data.albums;
     
     // Calcular el valor de `next` para el siguiente conjunto de álbumes
@@ -34,6 +33,13 @@ router.get('/', async (req, res) => {
     res.status(500).send('Error al obtener álbumes de la API');
   }
 });
+
+router.post('/', async (req, res) => {
+    let newAlbum = req.body;
+    let url = '/albums';
+    await apiClient.post(url, newAlbum);
+    res.redirect(req.get('referer'));
+  }); 
 
 
 module.exports = router;
